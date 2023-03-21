20b6e0429cca5e14caac56b55f3d989d07b5f5b1
Moises Marinho <moisesmarinhosilva87@gmail.com>
2023-03-21 15:17:19 -0300

    Exerciciofeito

---
+++ exercicio2notas.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class exercicio2notas {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Nota: ");
+        nota = scan.nextInt();
+
+        while (nota < 0 | nota > 10) {
+            System.out.println("Nota Invalida! Digite Novamente");
+            nota = scan.nextInt();
+        }
+    }
+
+}
